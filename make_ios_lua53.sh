154da95421c2bd4b213f393d2c6119a8aee7dc29
GALABEN <KMUS1997@163.com>
2024-12-04 02:35:58 +0800

    Set iOS deployment target to 12.0 in build script

--- make_ios_lua53.sh
+++ make_ios_lua53.sh
@@ -1,7 +1,14 @@
+#!/bin/bash
+# 创建构建目录并进入
 mkdir -p build_ios && cd build_ios
-cmake -DCMAKE_TOOLCHAIN_FILE=../cmake/ios.toolchain.cmake -DPLATFORM=OS64 -GXcode ../
+
+# 运行 cmake 配置命令，并设置最低 iOS 部署版本为 12.0
+cmake -DCMAKE_TOOLCHAIN_FILE=../cmake/ios.toolchain.cmake \
+      -DPLATFORM=OS64 \
+      -DCMAKE_OSX_DEPLOYMENT_TARGET=12.0 \   # 设置最低部署版本为 12.0
+      -GXcode ../
 cd ..
-cmake --build build_ios --config Release
+cmake --build build_ios --config Release --verbose
 mkdir -p plugin_lua53/Plugins/iOS/
 cp build_ios/Release-iphoneos/libxlua.a plugin_lua53/Plugins/iOS/libxlua.a 
 
